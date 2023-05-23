3ca36bb44954473c377848a21bf85c19c139f53d
Allen Wong <73718073+sfc-gh-awong@users.noreply.github.com>
2023-05-23 13:22:10 -0400

    Update big-data-unload-load.sql
    target table now named tpcds_target_copy_into

--- big-data-unload-load.sql
+++ big-data-unload-load.sql
@@ -20,7 +20,7 @@ Logging
                                                     x2large     1m25s           762                 2m
     store_sales     28B         1.3TB (4TB Uncom)   x4large     5m19s           6901                5m30s          April 2021       
     store_sales     28B         1.3TB (4TB Uncom)   x4large     4m6s            6901                4m11s          May 2023 
-    store_sales     28B         1.3TB (4TB Uncom)   x5large     2m14s           8053                2m22s          May 2023 
+    store_sales     28B         1.3TB (4TB Uncom)   x5large     2m14s           8053                2m20s          May 2023 
     store_sales     28B         1.3TB (4TB Uncom)   x6large     1m35s           8000                1m16s          May 2023 
 
 
@@ -37,13 +37,15 @@ Use Cases for this script:
 
 --PART 1: SETUP
 --PART 2: COPY INTO STAGE (BLOB STORAGE)
---PART 3: SNOWPIPE
---PART 4: COPY INTO Snowflake
+--PART 3: SNOWPIPE into tpcds_target_snowpipe
+--PART 4: COPY INTO tpcds_target_copy_into
 
 
     
 
 */
+
+
 
 ----------------------------------------------------------------------------------------------------------
 --PART 1: SETUP
@@ -61,7 +63,7 @@ Use Cases for this script:
     
     --CHANGE THE SOURCE TABLE AS NECESSARY
     create view tpcds.source_vw as
-    select * from snowflake_sample_data.tpcds_sf10tcl.store_sales;     //customer (65M)  //store_returns (2.9B)    //store_sales (28B)
+    select * from snowflake_sample_data.tpcds_sf10tcl.customer;     //customer (65M)  //store_returns (2.9B)    //store_sales (28B)
     
     select top 300 * from tpcds.source_vw;
 
@@ -72,19 +74,19 @@ Use Cases for this script:
         create transient table tpcds.tpcds_target_snowpipe as select * from tpcds.source_vw limit 0;
         
         --copy into
-        drop table if exists tpcds.tpcds_target;
-        create transient table tpcds.tpcds_target as select * from tpcds.source_vw limit 0;
+        drop table if exists tpcds.tpcds_target_copy_into;
+        create transient table tpcds.tpcds_target_copy_into as select * from tpcds.source_vw limit 0;
         
         --counts
         select count(*), 'source' location from tpcds.source_vw union all
         select count(*), 'target_snowpipe' location from tpcds.tpcds_target_snowpipe union all
-        select count(*), 'target' location from tpcds.tpcds_target;
+        select count(*), 'target_copy_into' location from tpcds.tpcds_target_copy_into;
 
         --Create Snowpipe
-        drop pipe if exists play_db.tpcds.pipe_171;
+        drop pipe if exists play_db.tpcds.pipe_171_snow_customer;
         
         //PIPE is a wrapper around COPY INTO
-        create pipe play_db.tpcds.pipe_171 auto_ingest = true as 
+        create pipe play_db.tpcds.pipe_171_snow_customer auto_ingest = true as 
         copy into tpcds.tpcds_target_snowpipe
             from @S3_db.public.S3_stage/tpcds/
                 file_format = (type = csv
@@ -108,12 +110,12 @@ Use Cases for this script:
 --PART 2: COPY INTO STAGE (BLOB STORAGE)
 
     --SIZE UP: small (customer)    xlarge (store_returns)    x4large (store_sales)
-    alter warehouse compute_wh resume;
+    -- alter warehouse compute_wh resume;
     alter warehouse compute_wh set warehouse_size = 'small' wait_for_completion = true;
     
     
             --copy into <stage> will UNLOAD from Snowflake
-                copy into @S3_db.public.S3_stage/tpcds/snow from 
+                copy into @S3_db.public.S3_stage/tpcds/snow_customer from 
                     (select * from tpcds.source_vw)
                     max_file_size = 262144000   //250MB
                     overwrite = true
@@ -141,17 +143,16 @@ Use Cases for this script:
 
         
 ----------------------------------------------------------------------------------------------------------
---PART 3: SNOWPIPE
+--PART 3: SNOWPIPE into tpcds_target_snowpipe
 
     --counts
     select count(*), 'source' location from tpcds.source_vw union all
     select count(*), 'target_snowpipe' location from tpcds.tpcds_target_snowpipe union all
-    select count(*), 'target' location from tpcds.tpcds_target;
+    select count(*), 'target_copy_into' location from tpcds.tpcds_target_copy_into;
 
-    select system$pipe_status('pipe_171');      
+    select system$pipe_status('pipe_171_snow_customer');      
 
     select top 300 * from tpcds_target_snowpipe;
-
 
 
 
@@ -161,12 +162,12 @@ Use Cases for this script:
         
         
 ----------------------------------------------------------------------------------------------------------
---PART 4: COPY INTO Snowflake
+--PART 4: COPY INTO tpcds_target_copy_into
     --SIZE UP: small (customer)    xlarge (store_returns)    x4large (store_sales)
-    alter warehouse compute_wh resume;
+    -- alter warehouse compute_wh resume;
     alter warehouse compute_wh set warehouse_size = 'small' wait_for_completion = true;
 
-        copy into tpcds.tpcds_target 
+        copy into tpcds.tpcds_target_copy_into 
         from @S3_db.public.S3_stage/tpcds/ 
         file_format = (type = csv
             field_optionally_enclosed_by='"'        //double-quote strings 
@@ -180,14 +181,29 @@ Use Cases for this script:
     --counts
     select count(*), 'source' location from tpcds.source_vw union all
     select count(*), 'target_snowpipe' location from tpcds.tpcds_target_snowpipe union all
-    select count(*), 'target' location from tpcds.tpcds_target;
+    select count(*), 'target_copy_into' location from tpcds.tpcds_target_copy_into;
+
+
+-----------------------------------------------------
+--CLEANUP
+
+
+truncate table tpcds.tpcds_target_snowpipe;
+truncate table tpcds.tpcds_target_copy_into;
+
+show pipes;
+drop pipe if exists pipe_171_snow_customer;
+
+ls @S3_db.public.S3_stage/tpcds;
+remove @S3_db.public.S3_stage/tpcds;
+
 
 /*
 RECAP
 
-PART 1: SETUP
-PART 2: COPY INTO STAGE (BLOB STORAGE)
-PART 3: SNOWPIPE
-PART 4: COPY INTO Snowflake
+--PART 1: SETUP
+--PART 2: COPY INTO STAGE (BLOB STORAGE)
+--PART 3: SNOWPIPE into tpcds_target_snowpipe
+--PART 4: COPY INTO tpcds_target_copy_into
 
 */
